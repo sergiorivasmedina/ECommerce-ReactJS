dd8f7aeea6216b13ff428be662770c7019e4a964
Rivas Medina <sergio.rivas_medina@siemens.com>
2019-10-05 13:26:04 -0500

    footer para todas las paginas

--- src/components/Vegefoods/Footer.js
+++ src/components/Vegefoods/Footer.js
@@ -15,12 +15,11 @@ class Footer extends React.Component {
           <div className="row mb-5">
             <div className="col-md">
               <div className="ftco-footer-widget mb-4">
-                <h2 className="ftco-heading-2">Vegefoods</h2>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+                <h2 className="ftco-heading-2">Agroferias Campesinas</h2>
+                <p>Fueron diseñadas para revalorar la agricultura familiar, sus productores y la biodiversidad del Perú. La feria empezó como proyecto en el 2012 y fue desarrollada por la Sociedad Peruana de Gastronomía – APEGA y financiado por el BID FOMIN.</p>
                 <ul className="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                  <li className="ftco-animate"><a href="#"><span className="icon-twitter"></span></a></li>
-                  <li className="ftco-animate"><a href="#"><span className="icon-facebook"></span></a></li>
-                  <li className="ftco-animate"><a href="#"><span className="icon-instagram"></span></a></li>
+                  <li><a href="https://www.facebook.com/agroferiasperu/"><span className="icon-facebook"></span></a></li>
+                  <li><a href="https://www.instagram.com/agroferiascampesinas/"><span className="icon-instagram"></span></a></li>
                 </ul>
               </div>
             </div>
@@ -28,38 +27,23 @@ class Footer extends React.Component {
               <div className="ftco-footer-widget mb-4 ml-md-5">
                 <h2 className="ftco-heading-2">Menu</h2>
                 <ul className="list-unstyled">
-                  <li><a href="#" className="py-2 d-block">Shop</a></li>
-                  <li><a href="#" className="py-2 d-block">About</a></li>
-                  <li><a href="#" className="py-2 d-block">Journal</a></li>
-                  <li><a href="#" className="py-2 d-block">Contact Us</a></li>
+                  <li><a href="/Ferias" className="py-2 d-block">Ferias</a></li>
+                  <li><a href="/Tiendas" className="py-2 d-block">Tiendas</a></li>
+                  <li><a href="/Productos" className="py-2 d-block">Productos</a></li>
+                  <li><a href="/Canasta" className="py-2 d-block">Canasta</a></li>
+                  <li><a href="/Perfil" className="py-2 d-block">Perfil de Usuario</a></li>
                 </ul>
               </div>
             </div>
-            <div className="col-md-4">
-              <div className="ftco-footer-widget mb-4">
-                <h2 className="ftco-heading-2">Help</h2>
-                <div className="d-flex">
-                  <ul className="list-unstyled mr-l-5 pr-l-3 mr-4">
-                    <li><a href="#" className="py-2 d-block">Shipping Information</a></li>
-                    <li><a href="#" className="py-2 d-block">Returns &amp; Exchange</a></li>
-                    <li><a href="#" className="py-2 d-block">Terms &amp; Conditions</a></li>
-                    <li><a href="#" className="py-2 d-block">Privacy Policy</a></li>
-                  </ul>
-                  <ul className="list-unstyled">
-                    <li><a href="#" className="py-2 d-block">FAQs</a></li>
-                    <li><a href="#" className="py-2 d-block">Contact</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+            
             <div className="col-md">
               <div className="ftco-footer-widget mb-4">
-                <h2 className="ftco-heading-2">Have a Questions?</h2>
+                <h2 className="ftco-heading-2">¿Tiene preguntas?</h2>
                 <div className="block-23 mb-3">
                   <ul>
-                    <li><span className="icon icon-map-marker"></span><span className="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+                    <li><span className="icon icon-map-marker"></span><span className="text">Avenida Brasil cuadra 32, Lima, Peru</span></li>
                     <li><a href="#"><span className="icon icon-phone"></span><span className="text">+2 392 3929 210</span></a></li>
-                    <li><a href="#"><span className="icon icon-envelope"></span><span className="text">info@yourdomain.com</span></a></li>
+                    <li><a href="#"><span className="icon icon-envelope"></span><span className="text">hola@agroferias.com</span></a></li>
                   </ul>
                 </div>
               </div>
