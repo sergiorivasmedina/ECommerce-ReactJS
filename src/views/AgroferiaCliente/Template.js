93178a629c1231bc5c8ab10b5e656c5f8027ee2e
Carla Cachis <carla.cachis@pucp.pe>
2019-10-04 19:37:43 -0500

    :hankey: Diseño de Menu. Pendiente fijar rutas - Carla

--- src/views/AgroferiaCliente/Template.js
+++ src/views/AgroferiaCliente/Template.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navigator from '../../components/Vegefoods/Navigator';
+import Menu from '../../components/AgroferiaCliente/Menu';
 import Product from '../../components/Vegefoods/Product';
 import Footer from '../../components/Vegefoods/Footer';
 import Subscribe from '../../components/Vegefoods/Subscribe';
@@ -11,7 +12,7 @@ class Template extends React.Component {
     render() {
         return (
             <div className="Template">
-                <Navigator />
+                <Menu/>
                 <Heading title="Template View" imageUrl="images/bg_1.jpg" />
 
                 <section className="ftco-section">
