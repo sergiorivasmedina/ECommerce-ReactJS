91aa5dbd1d4f0b1b7b8adfab4de7cb212ddc436a
Daniela <danielaa.296@hotmail.com>
2019-10-05 12:03:49 -0500

    ShopDescription

--- src/components/AgroferiaCliente/TemplateComponent.js
+++ src/components/AgroferiaCliente/TemplateComponent.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
+import ShopDescription from './ShopDescription'
 
 export default class TemplateComponent extends Component {
     render() {
         return (
+          <div>
             <div className="col-md-3 text-center d-flex align-self-stretch ">
                 <div className="media block-6 services mb-md-0 mb-4">
                   <div className="icon bg-color-4 active d-flex justify-content-center align-items-center mb-2">
@@ -13,6 +15,8 @@ export default class TemplateComponent extends Component {
                     <span>{this.props.description}</span>
                   </div>
                 </div>
+              </div>
+              <ShopDescription shopName="Papa y camote" shopDetail="Vendemos tubérculos como camote, yuca, y más de 20 variedades de papa." imageUrl="./images/bg_1.jpg"></ShopDescription>
               </div>
         )
     }
