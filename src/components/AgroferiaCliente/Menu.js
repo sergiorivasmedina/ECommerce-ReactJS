040e16c958ebab59f8638ddd94f17e92dd20aa8f
Carla Cachis <carla.cachis@pucp.pe>
2019-10-05 16:00:29 -0500

    :art: menu terminado

--- src/components/AgroferiaCliente/Menu.js
+++ src/components/AgroferiaCliente/Menu.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
+
 class Menu extends React.Component {
     constructor(props) {
         super(props);
@@ -9,23 +11,26 @@ class Menu extends React.Component {
 
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+            <nav className="navbar navbar-expand-lg  bg-pink ftco-navbar-light" id="ftco-navbar">
                 <div className="container">
-                    <h5> {this.state.value}</h5>
+
+                    <img className="img-fluid" src="images/AgroferiaCliente/logoap.png" alt="Colorlib Template" />    
+            
+                    <h5 className="white"> {this.state.value}</h5>
 
                     <div className="collapse navbar-collapse" id="ftco-nav">
                         <ul className="navbar-nav ml-auto">
 
-                            <li className="nav-item"><label className="nav-link"><Link to="/Ferias" >FERIAS</Link></label></li>
-                            <li className="nav-item"><label className="nav-link"><Link to="/Tiendas">TIENDAS</Link></label></li>
-                            <li className="nav-item"><label className="nav-link"><Link to="/Productos">PRODUCTOS</Link></label></li>
+                            <li className="nav-item" ><label className="carla nav-link"><Link to="/Ferias" ><span className="white">FERIAS</span></Link></label></li>
+                            <li className="nav-item"><label className="nav-link"><Link to="/Tiendas"><span className="white">TIENDAS</span></Link></label></li>
+                            <li className="nav-item"><label className="nav-link"><Link to="/Productos"><span className="white">PRODUCTOS</span></Link></label></li>
 
-                            
+
                             {/* ACTIVAR PARA SIGUIENTE SPRINT <li className="nav-item"><label className="nav-link"><Link to="/Map">MAPS</Link></label></li>
                             <li className="nav-item"><label className="nav-link"><Link to="/Calendar">CALENDARIO</Link></label></li> */}
-                            
-                            <li className="nav-item cta cta-colored "><label className="nav-link"><Link to="/Canasta"><span className="icon-shopping_basket"></span>[0]</Link></label></li>
-                            <li className="nav-item cta cta-colored"><label className="nav-link"><Link to="/Perfil"><span className="icon-person"  ></span></Link></label></li>
+
+                            <li className="nav-item cta cta-colored "><label className="nav-link"><Link to="/Canasta"><span className="icons icon-shopping_basket"></span><span className="white">[0]</span></Link></label></li>
+                            <li className="nav-item cta cta-colored "><label className="nav-link"><Link to="/Perfil"><span className="icons icon-person"></span></Link></label></li>
 
                         </ul>
                     </div>
