b234bf0d23b1ed18368a0139646e84259001eaa5
Carla Cachis <carla.cachis@pucp.pe>
2019-10-05 16:22:49 -0500

    menu de ferias

---
+++ src/components/AgroferiaCliente/MenuFairsComponent.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: 'AGROFERIAS CAMPESINAS' };
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg  bg-pink ftco-navbar-light" id="ftco-navbar">
+                <div className="container">
+
+                    <img className="img-fluid" src="images/AgroferiaCliente/logoap.png" alt="Colorlib Template" />    
+            
+                    <h5 className="white"> {this.state.value}</h5>
+
+                    <div className="collapse navbar-collapse" id="ftco-nav">
+                        <ul className="navbar-nav ml-auto">
+
+                          
+                            <li className="nav-item cta cta-colored "><label className="nav-link"><Link to="/Perfil"><span className="icons icon-person"></span></Link></label></li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+
+}
+
+export default Menu;
--- src/views/AgroferiaCliente/Fairs.js
+++ src/views/AgroferiaCliente/Fairs.js
@@ -1,41 +1,47 @@
 import React from 'react';
 import FairComponent from '../../components/AgroferiaCliente/FairComponent';
 import FooterComponent from '../../components/AgroferiaCliente/FooterComponent';
+import MenuFairComponent from '../../components/AgroferiaCliente/MenuFairsComponent';
 
 
 
 class Fairs extends React.Component {
-    render() {
-        return (
-        <section className="ftco-section">
-          <div className="container">
-            <div className="row justify-content-center mb-3 pb-3">
-              <div className="col-md-12 heading-section text-center ">
-                <span className="subheading">Nuestras Ferias</span>
-                <h2 className="mb-4">Hola Casero</h2>
-                <p>¿ En qué feria desea comprar?</p>
-              </div>
+  render() {
+    return (
+      <div className="Fairs">
+        <MenuFairComponent />
+
+      
+      <section className="ftco-section">
+
+        <div className="container">
+          <div className="row justify-content-center mb-3 pb-3">
+            <div className="col-md-12 heading-section text-center ">
+              <span className="subheading">Nuestras Ferias</span>
+              <h2 className="mb-4">Hola Casero</h2>
+              <p>¿ En qué feria desea comprar?</p>
             </div>
           </div>
-          <div className="container">
-            <div className="row">
+        </div>
+        <div className="container">
+          <div className="row">
 
-              <FairComponent name="Magdalena" imageUrl="images/product-1.jpg" />
+            <FairComponent name="Magdalena" imageUrl="images/product-1.jpg" />
 
-              <FairComponent name="Callao" imageUrl="images/product-2.jpg" />
+            <FairComponent name="Callao" imageUrl="images/product-2.jpg" />
 
-              <FairComponent name="Jesús María" imageUrl="images/product-3.jpg" />
+            <FairComponent name="Jesús María" imageUrl="images/product-3.jpg" />
 
-              <FairComponent name="San Borja" imageUrl="images/product-4.jpg" />
+            <FairComponent name="San Borja" imageUrl="images/product-4.jpg" />
 
-            </div>
           </div>
-          <br></br>
-          <FooterComponent />
-        </section>
-        
+        </div>
+        <br></br>
+        <FooterComponent />
+      </section>
+      </div>   
         );
-    }
+  }
 
 }
 
