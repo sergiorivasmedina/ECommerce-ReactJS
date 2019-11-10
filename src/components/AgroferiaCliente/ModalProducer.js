b54735ef88c68ed66cb9aa01fceba4c2d521acbf
JohanaGS <a20105430@pucp.pe>
2019-11-10 00:11:24 -0500

    :zap: cambios en el modal

--- src/components/AgroferiaCliente/ModalProducer.js
+++ src/components/AgroferiaCliente/ModalProducer.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import {Modal, Button} from 'react-bootstrap';
+import {Modal, Button,Card,Col,Row,Image} from 'react-bootstrap';
 
 class ModalProducer extends React.Component{
     render(){
         return(
-            <Modal show={this.props.status} onHide={()=>this.props.closeModal()}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Casero {this.props.name}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <img className="img-fluid" src={this.props.image} alt="Colorlib Template" />
-                    <h3>Descripción:</h3>
-                    <h6>{this.props.coment}</h6>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="primary" onClick={()=>this.props.closeModal()}>Cerrar</Button>
-                </Modal.Footer>
+            <Modal size="lg" aria-labelledby="contained-modal-title-vcenter"
+            centered
+            show={this.props.status} onHide={()=>this.props.closeModal()}>
+            <Modal.Body>
+                        <Row className="show-grid">
+                            <Col xs={12} md={6}>
+                                <Image src={this.props.image} alt="Colorlib Template" thumbnail />
+                            </Col>
+                        <Col xs={12} md={6}>
+                            <h3>Casero {this.props.name}</h3>
+                            <Card className="text-center">
+                            <Card.Header>Mi historia</Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                El motivo por el cual empece este negocio fue ...
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer >
+                                <Button size="lg" variant="primary" onClick={()=>this.props.closeModal()}>Volver</Button>
+                            </Card.Footer>
+                            </Card>
+                        </Col>
+                        </Row>
+            </Modal.Body>
             </Modal>
         )
     }
