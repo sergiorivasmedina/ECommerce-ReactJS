03dfc3613ae5ce3db023c640a61d8986ce73584f
YoluG <yoluanags@gmail.com>
2019-11-17 20:38:00 -0500

    Buscador superior con lista predefinida

--- src/components/AgroferiaCliente/SearchBarMenu.js
+++ src/components/AgroferiaCliente/SearchBarMenu.js
@@ -4,15 +4,24 @@ class SearchBarMenu extends React.Component {
     constructor(){
         super();
         this.state={
-          value:''
+            search:''
         }
     }
+
+    updateSearch(event){
+        this.setState({
+          search: event.target.value.substr(0,20)
+        });
+        this.props.updateSearch(event.target.value);
+    }
+
     render() {
         return (
             <form action="#" className="search-form" >
                 <div className="form-group">
                 <span className="icon ion-ios-search"></span>
-                <input margin-top="15px !important" type="text" className="form-control-2 form-control" placeholder="¿Que producto necesitas?"></input>
+                <input  margin-top="15px !important"  type="text" className="form-control-2 form-control" placeholder="¿Que producto necesitas?" 
+                value={this.props.search} onChange={this.updateSearch.bind(this)}></input>
                 </div>
             </form>
         );
