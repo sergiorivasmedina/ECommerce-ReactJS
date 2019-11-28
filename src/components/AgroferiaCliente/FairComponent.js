95b9ba4c5d254d30be52aef3db91ad5c24a3d90a
Rivas Medina <sergio.rivas_medina@siemens.com>
2019-11-28 01:15:48 -0500

    :bug: eliminar canasta al cambiar de feria

--- src/components/AgroferiaCliente/FairComponent.js
+++ src/components/AgroferiaCliente/FairComponent.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import APIFerias from '../../services/FairsService'
 
 class FairComponent extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      numProductosCanasta: 0
+    }
     this.routeChange = this.routeChange.bind(this);
   }
 
@@ -22,8 +27,38 @@ class FairComponent extends React.Component {
    
 
   async routeChange() {
+    var idFeria = sessionStorage.getItem("idFeria");
     await sessionStorage.setItem("idFeria", this.props.idFeria);
     console.log("this.props.idFeria",this.props.idFeria);
+
+    APIFerias.get('Despliegue/api/pedido/' + sessionStorage.getItem("idCliente"))
+      .then(res => {
+        //traer el detallePedido del idPedido, el cual es el actual
+        APIFerias.get('Despliegue/api/pedido/' + res.data.idPedido + '/detalle')
+            .then(response => {
+              this.setState.numProductosCanasta = response.data.length;
+              console.log("LONGITUD CANASTA: ", this.setState.numProductosCanasta);
+
+              if(idFeria != this.props.idFeria && this.setState.numProductosCanasta > 0){
+                console.log("DIFERENTE FERIA");
+          
+                APIFerias.delete('Despliegue/api/pedido/cliente/' + sessionStorage.getItem("idCliente")).then(res =>{
+                  console.log("SE ELIMINA LA CANASTA ACTUAL");
+                })
+          
+                Swal.fire({
+                  type: 'success',
+                  title: '¡Ha cambiado de feria!',
+                  text: '¡Se ha vaciado su canasta',
+              });
+              }
+                
+            })
+
+            
+      })
+
+
     let path = "/tiendas";
     this.props.history.push(path);
   }
